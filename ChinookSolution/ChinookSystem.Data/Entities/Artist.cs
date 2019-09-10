1b008abbe04088b344eaf6e9c99adee2ec47587b
mcaputolan1 <46543422+mcaputolan1@users.noreply.github.com>
2019-09-09 20:50:10 -0600

    Review of entity creation

---
+++ ChinookSolution/ChinookSystem.Data/Entities/Artist.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace ChinookSystem.Data.Entities
+{
+    [Table("Artists")]
+    public class Artist
+    {
+        [Key]
+        public int ArtistId { get; set; } //auto implemented property
+
+        //full implemented property
+        private string _Name;
+
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    _Name = null;
+                }
+                else
+                {
+                    _Name = value;
+                }//eof(end of if)
+            }
+        }//eop(end of property)
+
+        public virtual ICollection<Album> Albums { get; set; }
+
+    }//eoc(end of class)
+}//eon(end of namespace)
---
+++ ChinookSolution/ChinookSystem.Data/Entities/Genre.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace ChinookSystem.Data.Entities
+{
+    [Table("Genres")]
+    public class Genre
+    {
+        [Key]
+        public int GenreId { get; set; } //auto implemented property
+
+        //full implemented property
+        private string _Name;
+
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    _Name = null;
+                }
+                else
+                {
+                    _Name = value;
+                }//eof(end of if)
+            }
+        }
+    }
+}
